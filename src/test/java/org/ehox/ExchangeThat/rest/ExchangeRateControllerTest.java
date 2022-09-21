f2fcae3abc8c7a454ef1801564e9b83dc0fb4218
hoxhaedi <edi.hoxhalli@adidas.com>
2022-09-21 14:59:51 +0200

    added logging test for exceptions

--- src/test/java/org/ehox/ExchangeThat/rest/ExchangeRateControllerTest.java
+++ src/test/java/org/ehox/ExchangeThat/rest/ExchangeRateControllerTest.java
@@ -1,11 +1,16 @@
 package org.ehox.ExchangeThat.rest;
 
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
 import org.ehox.ExchangeThat.service.ExchangeRateService;
 import org.ehox.ExchangeThat.service.ServerExchangeRateResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mockito;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,7 +20,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
 import static org.ehox.ExchangeThat.service.ExchangeRateService.EXCHANGE_RATE_URL;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -31,12 +39,25 @@ public class ExchangeRateControllerTest {
     @InjectMocks
     private ExchangeRateService rateService;
 
+    Logger serviceLogger;
+    ListAppender<ILoggingEvent> listAppender;
+
+    @BeforeEach
+    public void setup(){
+        serviceLogger = (Logger) LoggerFactory.getLogger(ExchangeRateService.class);
+        listAppender = new ListAppender<>();
+        listAppender.setName("TEST");
+        listAppender.start();
+        serviceLogger.addAppender(listAppender);
+    }
+
     @Test
     @DisplayName("Get Single ExchangeRate Returns Bad Request when called with incorrect from-value")
     public void getSingleExchangeRateReturnsBadRequestIncorrectFrom() throws Exception {
         Mockito.when(restTemplate.getForEntity(EXCHANGE_RATE_URL, ServerExchangeRateResponse.class, "incorrect", "USD"))
                 .thenReturn(new ResponseEntity<>(new ServerExchangeRateResponse(), HttpStatus.BAD_REQUEST));
         this.mockMvc.perform(get("/exchange?from=incorrect&to=USD")).andExpect(status().isBadRequest());
+        assertLogging(ExchangeRateService.BAD_REQUEST_EXCEPTION);
     }
 
     @Test
@@ -45,5 +66,21 @@ public class ExchangeRateControllerTest {
         Mockito.when(restTemplate.getForEntity(EXCHANGE_RATE_URL, ServerExchangeRateResponse.class, "EUR", "incorrect"))
                 .thenReturn(new ResponseEntity<>(new ServerExchangeRateResponse(), HttpStatus.BAD_REQUEST));
         this.mockMvc.perform(get("/exchange?from=EUR&to=incorrect")).andExpect(status().isBadRequest());
+        assertLogging(ExchangeRateService.BAD_REQUEST_EXCEPTION);
+    }
+
+    @Test
+    @DisplayName("Get Single ExchangeRate Returns Bad Gateway when remote server returns 500")
+    public void getSingleExchangeRateReturnsBadGatewayIncorrectTo() throws Exception {
+        Mockito.when(restTemplate.getForEntity(EXCHANGE_RATE_URL, ServerExchangeRateResponse.class, "EUR", "USD"))
+                .thenReturn(new ResponseEntity<>(new ServerExchangeRateResponse(), HttpStatus.INTERNAL_SERVER_ERROR));
+        this.mockMvc.perform(get("/exchange?from=EUR&to=USD")).andExpect(status().isBadGateway());
+        assertLogging(ExchangeRateService.REMOTE_SERVER_EXCEPTION);
+    }
+
+    private void assertLogging(String badRequestException) {
+        List<ILoggingEvent> logsList = this.listAppender.list;
+        assertEquals(badRequestException, logsList.get(0)
+                .getMessage());
     }
 }
